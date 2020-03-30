3afb13b2860b668729e73d7169e833dae30e6d9a
Matheus Suffi <matheus_suffi@live.com>
2020-03-30 18:27:25 -0300

    ajustes

--- app.js
+++ app.js
@@ -91,6 +91,7 @@ app.post('/login', function (req, res) {
 })
 
 app.get('/teste', (req, res) => {
+    console.log('teste')
     res.send('teste');
 })
 //Função que recebe os parametros do login do professor/admin e retorna os dados do usuario
