dc83cbd08cef633f425bd01591dbc51a56d72005
Matheus Suffi <matheus_suffi@live.com>
2020-06-08 21:02:43 -0300

    ajustes

--- app.js
+++ app.js
@@ -952,21 +952,29 @@ app.post('/enviaComentario', function (req, res) {
 app.post('/get_tema', function (req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*")
     try{
-        let currentWeek = currentWeekNumber()
-        //let currentDay = new Date().getDate()
-        //let currentMonth = new Date().getMonth() + 1
-        let currentYear = new Date().getFullYear()
-        let queryTema = `SELECT * FROM tb_tema WHERE ano = '${currentYear}' AND semana = '${currentWeek}' ORDER BY id DESC limit 1`
-        console.log(queryTema)
+        //Antes de mais nada eu vejo se o usuario que fez a requisição já não enviou 2 redações
+        let queryRedacao = `SELECT COUNT(*) as contador FROM tb_redacao WHERE id_aluno = '${req.body.id}'`
         connection.query(queryTema,(err, data) => {
-            console.log(JSON.stringify(data))
-            if (err){
-                console.log(err)
-                res.send( {'status':'erro','desc':err} )
+            if(data[0]['contador'] >= 2){
+                res.send( {'status':'limit_redacao','desc':err} )
             }else{
-                res.send({'status':'ok','desc':data})
+                let currentWeek = currentWeekNumber()
+                //let currentDay = new Date().getDate()
+                //let currentMonth = new Date().getMonth() + 1
+                let currentYear = new Date().getFullYear()
+                let queryTema = `SELECT * FROM tb_tema WHERE ano = '${currentYear}' AND semana = '${currentWeek}' ORDER BY id DESC limit 1`
+                console.log(queryTema)
+                connection.query(queryTema,(err, data) => {
+                    console.log(JSON.stringify(data))
+                    if (err){
+                        console.log(err)
+                        res.send( {'status':'erro','desc':err} )
+                    }else{
+                        res.send({'status':'ok','desc':data})
+                    }
+                })    
             }
-        })        
+        })    
     }catch(err){
         console.log('caiu aqui3' + err)
         res.send({'status':'erro','desc':'erro'})
